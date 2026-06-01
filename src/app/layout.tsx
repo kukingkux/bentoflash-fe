bc42329121b3d307517860b9508dc5e7b377fdf2
kukingkux <cbmamangbae@gmail.com>
2026-06-01 22:20:10 +0700

    Refactor global styles and layout for improved font handling and structure
    - Updated font definitions in globals.css to use system fonts.
    - Removed unused Geist font imports from layout.tsx.
    - Structured layout.tsx for better readability and maintainability.
    - Added tailwind.config.ts for centralized font configuration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import { UserProvider } from "@/context/UserContext";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Kantina",
@@ -24,11 +13,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">
+    <html lang="en" className="h-full antialiased">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Light:opsz,wght,FILL,GRAD@24,100,0,0"
+        />
+      </head>
+      <body className="min-h-full flex flex-col font-sans">
         <UserProvider>{children}</UserProvider>
       </body>
     </html>
